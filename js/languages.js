17264a1dba8ad21883c0d1f1292e1f7f87b1fa66
Lucas Conti <lucasger78@hotmail.com>
2023-06-21 16:29:42 -0300

    Actaulización Lenguajes 20-06-23

--- js/languages.js
+++ js/languages.js
@@ -3,7 +3,7 @@ const flagsElement = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
-  const requestJson = await fetch(`languages/${language}.json`);
+  const requestJson = await fetch(`js/${language}.json`);
   const texts = await requestJson.json();
 
   for (const textToChange of textsToChange) {
